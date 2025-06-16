dcd561e7c9f325fafdb1042a7565f4f034534dc9
Mike <mike.beplus@gmail.com>
2025-06-16 08:39:09 +0700

    Update func BBACKUP_Upload_File_Backup

--- inc/ajax.php
+++ inc/ajax.php
@@ -37,18 +37,57 @@ if(! function_exists('BBACKUP_Upload_File_Backup')) {
    * @since 1.0.0 
    */
   function BBACKUP_Upload_File_Backup() {
+    /**
+      * Fix issue security
+      * verify only admin can access
+      */
+      if( ! current_user_can( 'manage_options' ) ) {
+        wp_send_json_error( 'You are not authorized to access this page.' );
+        exit();
+    }
+    /** End fix issue security */
+    
     @ini_set( 'upload_max_size' , '512M' );
     @ini_set( 'post_max_size' , '512M' );
 
+    /**
+     * file type verify
+     */
     $file = $_FILES['file'];
+    $allowed_types = array('application/zip', 'application/x-zip-compressed', 'application/zip+octet-stream');
+    $max_size = 150 * 1024 * 1024; // 150MB
 
+    /** verify file type */
+    if (!in_array($file['type'], $allowed_types)) {
+      wp_send_json_error('Invalid file type');
+      exit();
+    }
+
+    /** verify file size */
+    if ($file['size'] > $max_size) {
+      wp_send_json_error(sprintf('File size is too large, max size is %sMB', ($max_size / 1024 / 1024)));
+      exit();
+    }
+
+    // wp_send_json_error( 'test' );
+    // exit();
+    
     $upload_overrides = array( 
       'test_form' => false,
       'mimes' => array(
         'zip' => array('application/zip+octet-stream'),
       ),
     );
-    $movefile = wp_handle_upload( $file, $upload_overrides );
+
+    // wp_send_json_error( [$file, $upload_overrides] );
+    // exit();
+
+    $movefile = wp_handle_upload($file, array(
+        'test_form' => false,
+        'mimes' => array(
+            'zip' => 'application/zip'
+        )
+    ));
 
     if ( $movefile ) {
       wp_send_json_success( $movefile );
@@ -60,5 +99,5 @@ if(! function_exists('BBACKUP_Upload_File_Backup')) {
   }
 
   add_action( 'wp_ajax_BBACKUP_Upload_File_Backup', 'BBACKUP_Upload_File_Backup' );
-  add_action( 'wp_ajax_nopriv_BBACKUP_Upload_File_Backup', 'BBACKUP_Upload_File_Backup' );
+  // add_action( 'wp_ajax_nopriv_BBACKUP_Upload_File_Backup', 'BBACKUP_Upload_File_Backup' );
 }
